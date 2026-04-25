37f68d6484d3a8c551f181ed23383dccc728aa29
Viharika <chandraviharika@gmail.com>
2026-04-25 14:26:41 +0000

    Virtual Try-On - SCHP + OOTDiffusion pipeline

---
+++ preprocess/openpose/config.py
@@ -0,0 +1,2 @@
+body_estimation_model = '/content/OOTDiffusion/checkpoints/ootd/checkpoints/openpose/ckpts/body_pose_model.pth'
+hand_estimation_model = '/content/OOTDiffusion/checkpoints/ootd/checkpoints/openpose/ckpts/body_pose_model.pth'
